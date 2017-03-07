e78e531e972b0468c3ac885246b4db7207cc6342
vonKristoff <mrjcnicolas@gmail.com>
2017-03-07 15:39:40 +0000

    add raw project base files

---
+++ _raw/project/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    body: {
+        title: "__project",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        title: "__project",
+        description: ""
+    },
+    script: {
+        use: false,
+        src: "js/__project.js"
+    }
+}
