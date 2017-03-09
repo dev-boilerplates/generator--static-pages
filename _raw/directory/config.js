1ca4774b12fc51f05da01142f9f9801b3ca3de27
vonKristoff <mrjcnicolas@gmail.com>
2017-03-09 15:34:49 +0000

    rename dir

---
+++ _raw/directory/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    body: {
+        title: "__id",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        title: "__id",
+        description: ""
+    },
+    script: {
+        use: false,
+        src: "js/__id.js"
+    }
+}
