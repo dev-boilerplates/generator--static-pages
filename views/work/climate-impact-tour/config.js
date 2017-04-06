18dc0fbf219b9fe18908817f71525abf1126212a
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:30:00 +0100

    add arctic page

---
+++ views/work/climate-impact-tour/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    body: {
+        title: "Arctic Climate Impact Tour",
+        client: "Cambridge University",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        href: "climate-impact-tour",
+        title: "The Climate Impact Tour",
+        description: "ScanLAB Projects joined expeditions to the Fram Strait, North West of Svalbard, Norway to capture in forensic detail the 3D form of Arctic Sea Ice.",
+        active: true,
+        tags: []
+    },
+    script: {
+        use: false,
+        src: "/js/climateimpacttour.js"
+    },
+    images: []
+}
