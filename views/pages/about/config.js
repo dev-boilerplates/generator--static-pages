d15e41eaedb14b994f8778fd20e7c11041da6e22
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:13:49 +0100

    add about page

---
+++ views/pages/about/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    body: {
+        title: "ScanLAB Projects is an award winning creative studio. We specialise in exploring the use of large scale 3D scanning in architecture and the creative industries.",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        title: "about",
+        description: "ScanLAB Projects is an award winning creative studio. We specialise in exploring the use of large scale 3D scanning in architecture and the creative industries."
+    },
+    script: {
+        use: false,
+        src: "/js/about.js"
+    },
+    images: [],
+    quotes: [{
+        text: "'A fantastic visual punch ... quite simply breathtaking'",
+        by: "Nicola Seare, Culture Show, BBC TWO"
+    },{
+        text: "'Apart from the exquisitely defined visual results, the scanning of 55/02 has provided us with an entirely new and powerful design tool.'",
+        by: "Bob Sheil, Director, Bartlett School of Architecture, UCL"
+    },{
+        text: "'Working with ScanLAB reduces the extreme effort of manual profiling of sea ice in the Arctic to a matter of hours. A full description of a floe can be carried away and every contour examined in full living detail'",
+        by: "Peter Wadhams, Professor of Ocean Physics and Head of the Polar Ocean Physics Group, Cambridge University"
+    }]
+}
