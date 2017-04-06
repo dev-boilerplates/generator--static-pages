b443d2bd019f5c9875081ea59faa1408ad07e5a9
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:31:21 +0100

    add a museum made digital page

---
+++ views/work/museum-made-digital/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    body: {
+        title: "A Museum Made Digital",
+        client: "The Sir John Soane Museum",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        href: "museum-made-digital",
+        title: "A Museum Made Digital",
+        description: "",
+        active: true,
+        tags: ['web']
+    },
+    script: {
+        use: false,
+        src: "/js/soanemuseum.js"
+    },
+    images: []
+}
