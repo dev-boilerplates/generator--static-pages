239796dc3cbc14b625c0e68143de8e0347fb0cc0
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:23:17 +0100

    add bartlett page

---
+++ views/work/bartlett-digital-exhibition/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    body: {
+        title: "Bartlett Digital Exhibition Apps",
+        client: "University College London",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        href: "bartlett-digital-exhibition",
+        title: "Bartlett Digital Exhibition Apps | University College London",
+        description: "Bartlett Digital Exhibition Apps, an interactive guide to the Bartlett Summer Shows",
+        active: true,
+        tags: []
+    },
+    script: {
+        use: false,
+        src: "/js/bartlettdigitalexhibition.js"
+    },
+    images: []
+}
