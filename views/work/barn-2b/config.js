7da6d31618934d1d43226b15ab86226c024dd5f6
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:22:51 +0100

    add barn-2b page

---
+++ views/work/barn-2b/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    body: {
+        title: "barn2b",
+        client: "University for the Creative Arts",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        href: "barn-2b",
+        title: "Barn 2B | UCA",
+        description: "The Barn 2B ipad app is an interactive digital document containing 3D spatial information about a historic structure in Lithuania.",
+        active: true,
+        tags: []
+    },
+    script: {
+        use: false,
+        src: "/js/barn2b.js"
+    },
+    images: []
+}
