cb9f63f99a1eabad0f227d3515b4bb3b10adf5f0
vonKristoff <mrjcnicolas@gmail.com>
2017-04-06 14:14:06 +0100

    add work page

---
+++ views/pages/work/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    body: {
+        title: "work",
+        label: "",
+        summary: ""
+    },
+    meta: {
+        title: "work",
+        description: ""
+    },
+    script: {
+        use: true,
+        src: "/js/work.js"
+    },
+    tags: ['vr', 'digital', 'web', 'film'],
+    images: [],
+    projects: require('./config.work.json'),
+    order: [
+        'italysinvisiblecities',
+        'museummadedigital',
+        'pompeiinewsecretsrevealed',
+        'collapse',
+        'dreamlifeofdriverlesscars',
+        'falstonecountryshow',
+        'ibeyi',
+        'phantom',
+        'more4',
+        'shippinggalleries',
+        'horizontalhumans',
+        'savagebeauty',
+        'frozenrelic',
+        'barn2b',
+        'forensicarchitecture',
+        'bartlettdigitalexhibition',
+        'lordnelsonstrafalgarcoat',
+        'noise',
+        'frozenrelic',
+        'pennsylvaniacastle',
+        'climateimpacttour'
+    ]
+}
